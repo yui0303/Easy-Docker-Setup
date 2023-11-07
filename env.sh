9efd884fba101016ad47ab6ad3a62892d2efa7fb
yui0303 <iampeter62@gmail.com>
2023-11-07 08:44:31 +0000

    feat: Update script. Add jupyter option

--- env.sh
+++ env.sh
@@ -1,8 +1,9 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 Username=$(whoami)
-ImageName=$(echo $Username"_img")
-ContainerName=$(echo $Username"_container")
+DirName=$(basename $(pwd) | tr '[:upper:]' '[:lower:]')
+ImageName=$(echo $Username"_img_"$DirName)
+ContainerName=$(echo $Username"_container_"$DirName)
 JupyterPort=8333
 
 if [ $# -eq 0 ] ; then 
@@ -35,7 +36,7 @@ if [ $1 == "start" ] ; then
     
 elif [ $1 == "stop" ] ; then # stop the container
     if ! docker stop $ContainerName > /dev/null 2>&1 ; then
-        echo "There is a unexpected wrong when stop the '$ContainerName'"
+        echo "There is an unexpected wrong when stop the '$ContainerName'"
     elif [ $(docker ps | grep -w $ContainerName | wc -l) -eq 0 ] ; then
         echo "The '$ContainerName' is not running."
     else
@@ -43,15 +44,19 @@ elif [ $1 == "stop" ] ; then # stop the container
     fi
 elif [ $1 == "rm" ] ; then # remove the container
     if ! docker rm "$ContainerName" > /dev/null 2>&1 ; then
-        echo "There is a unexpected wrong when removing '$ContainerName'"
+        echo "There is an unexpected wrong when removing '$ContainerName'"
     else
         echo "Remove the '$ContainerName' successfully!"
     fi
 elif [ $1 == "rmi" ] ; then # remove the image
     if ! docker rmi "$ImageName" > /dev/null 2>&1 ; then
-        echo "There is a unexpected wrong when removing '$ImageName'"
+        echo "There is an unexpected wrong when removing '$ImageName'"
     else
         echo "Remove the '$ImageName' successfully!"
+    fi
+elif [ $1 == "jupyter" ] ; then # open jupyter notebook
+    if ! jupyter notebook --ip=0.0.0.0 --port=$JupyterPort --no-browser ; then
+        echo "There is an unexpected wrong when opening jupyter notebook"
     fi
 elif [ $1 == "--help" ] ; then
     echo ./env "parameter"
@@ -59,6 +64,7 @@ elif [ $1 == "--help" ] ; then
     echo "stop : to stop the container"
     echo "rm : remove the conatiner"
     echo "rmi : remove the iamge"
+    echo "jupyter: run the jupyter notebook in port $JupyterPort"
 else 
     echo "Wrong parameter!"
     echo "./env.sh --help for more infomation"
